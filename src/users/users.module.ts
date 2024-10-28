ab42b990f41cb6b73d9ff216351acfa9c698ef54
hissoune <khalidhissoune962@gmail.com>
2024-10-28 20:11:08 +0100

    Fix : fix the user modul to add to it the modul of the settings

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,14 +3,24 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { User, UserSchema } from "src/schemas/user.schema";
 import { UsersService } from "./users.service";
 import { usersControllser } from "src/users/users.controller";
+import { UserSchemaSettings, userSettings } from "src/schemas/userSettings.Schema";
 
 
 @Module({
     imports:[
-        MongooseModule.forFeature([{
+        MongooseModule.forFeature([
+            
+            {
             name:User.name,
             schema:UserSchema
-        }])
+           },
+           {
+            name: userSettings.name,
+            schema:UserSchemaSettings,
+           }
+           
+    
+    ])
     ],
     providers:[UsersService],
     controllers:[usersControllser]
