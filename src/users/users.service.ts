819b0b53069954defab2daee3890ce8db02672fa
hissoune <khalidhissoune962@gmail.com>
2024-10-28 20:42:54 +0100

    Fix : fix the user service to have the ability to add settings while create

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,6 +4,7 @@ import { Model } from "mongoose";
 import { User } from "src/schemas/user.schema";
 import { createUserDto } from "./dto/createUser.dto.user";
 import { updateUserDto } from "./dto/upadateUser.dto";
+import { userSettings } from "src/schemas/userSettings.Schema";
 
 
 
@@ -13,21 +14,32 @@ export class UsersService {
 
 
     constructor(
-        @InjectModel(User.name) private userModele:Model<User>
+        @InjectModel(User.name) private userModele:Model<User>,
+        @InjectModel(userSettings.name) private userSettingsModele:Model<userSettings>
     ){}
 
-    createUser(createUserDto:createUserDto){
+   async createUser({settings,...createUserDto}:createUserDto){
+      if(settings){
+           const newSettings = new this.userSettingsModele(settings)
+         const savedSettings = await  newSettings.save()
+
+         const neUser = new this.userModele({
+            ...createUserDto,
+            settings:savedSettings._id,
+         });
+         return neUser.save();
+      }
       const newUser = new this.userModele(createUserDto)
 
       return newUser.save();
     }
 
     getUsers(){
-        return this.userModele.find();
+        return this.userModele.find().populate('settings');
     }
 
     getUserById(id:string){
-        return  this.userModele.findById(id);
+        return  this.userModele.findById(id).populate('settings');
     }
 
     updateUser (id:string, updateUserDto:updateUserDto){
